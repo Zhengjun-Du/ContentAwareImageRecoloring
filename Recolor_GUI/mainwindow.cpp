650b4e311316b37ded25f9cdab1c4f6a14b66cd3
Zheng-Jun Du <duzjqhu@aliyun.com>
2024-08-07 12:11:02 +0800

    Update mainwindow.cpp

--- Recolor_GUI/mainwindow.cpp
+++ Recolor_GUI/mainwindow.cpp
@@ -1,45 +1,31 @@
 #include<QWidget>
 #include<QPushButton>
 #include<QVBoxLayout>
-#include<QSlider>
 #include<QLabel>
-#include<Q3DSurface>
-#include<QtDataVisualization>
 #include<QDockWidget>
 #include<QTextEdit>
+#include<QLineEdit>
 #include<QListWidget>
 #include<QFileDialog>
 #include<QCheckBox>
-#include<QSplitter>
-#include<QSizePolicy>
-#include<QProgressDialog>
-#include<QLineEdit>
 #include <QGroupBox>
-
-#include "data.h"
-
 #include "mainwindow.h"
 #include "imagewidget.h"
 #include "openglwidget.h"
+#include "rgbwidget.h"
 #include "paletteviewwidget.h"
 #include "Qt-Color-Widgets\include\QtColorWidgets\color_dialog.hpp"
-
-// Setup UI
-
 #include <QColorDialog>
-
+#include "data.h"
 using namespace color_widgets;
 
-MainWindow::MainWindow(QWidget *parent) :
-    QMainWindow(parent)
-{
+MainWindow::MainWindow(QWidget *parent) :QMainWindow(parent){
     setWindowTitle(title);
 	setGeometry(100, 100, 520, 520);
-
-    data = new Data();
-
     QWidget *mainWidget = new QWidget();
     QVBoxLayout *mainLayout = new QVBoxLayout();
+	
+	data = new Data();
 
 	//top main menu==============================================================================================
     QHBoxLayout *FirstRowLayout = new QHBoxLayout();
@@ -56,7 +42,6 @@ MainWindow::MainWindow(QWidget *parent) :
 	FirstRowLayout->addWidget(EditNum);
 	FirstRowLayout->addWidget(CalcWeightsBtn);
 	mainLayout->addLayout(FirstRowLayout);
-	//top main menu==============================================================================================
 
 	//original image and recolored image=========================================================================
     ImageWidget *Recolored_image = new ImageWidget(true);
@@ -87,65 +72,18 @@ MainWindow::MainWindow(QWidget *parent) :
 	//import and export =========================================================================================
 	QHBoxLayout *secondRowLayout = new QHBoxLayout();
 	QPushButton *exportImageeBtn = new QPushButton("Export Image");
-	QPushButton* exportpsBtn = new QPushButton("Export Layers");//ÐÂ¼ÓµÄ
+	QPushButton* exportpsBtn = new QPushButton("Export Layers");
 	QPushButton *importPaletteButton = new QPushButton("Import Palette");
 	QPushButton *exportPaletteButton = new QPushButton("Export Palette");
 	secondRowLayout->addWidget(exportImageeBtn);
-	secondRowLayout->addWidget(exportpsBtn);//ÐÂ¼ÓµÄ
+	secondRowLayout->addWidget(exportpsBtn);
 	secondRowLayout->addWidget(importPaletteButton);
 	secondRowLayout->addWidget(exportPaletteButton);
-	//import and export =========================================================================================
 
-	//image play and vis=========================================================================================
-    /*QHBoxLayout *showImageDataLayout = new QHBoxLayout();
-    QCheckBox *showImageDataCheck= new QCheckBox();
-    QLabel *showImageDataLabel = new QLabel("25.0%");
-    QSlider *imagePreviewSlider = new QSlider(Qt::Horizontal);
-
-    showImageDataCheck->setCheckState(Qt::Checked);
-    showImageDataCheck->setText("Visualize Image Data Points");
-
-    imagePreviewSlider->setMinimum(0);
-    imagePreviewSlider->setMaximum(1000);
-    imagePreviewSlider->setValue(250);
-    showImageDataLabel->setBuddy(imagePreviewSlider);
-    showImageDataLabel->setWordWrap(false);
-
-    connect(imagePreviewSlider, &QSlider::valueChanged,
-            [=](const int &newValue){showImageDataLabel->setText(QString::number(newValue / 10., 'f', 1) + "%");} );
-
-    showImageDataLayout->addWidget(showImageDataCheck);
-    showImageDataLayout->addWidget(imagePreviewSlider);
-    showImageDataLayout->addWidget(showImageDataLabel);*/
-
-    QHBoxLayout *timeWindowLayout = new QHBoxLayout();
-    QSlider *timeWindowSlider = new QSlider(Qt::Horizontal);
-    timeWindowSlider->setMinimum(-3000);
-    timeWindowSlider->setMaximum(10000);
-
-    slider = new QSlider(Qt::Horizontal);
-    slider->setTickInterval(1);
-
-	QPushButton *autoPlayButton = new QPushButton("Play ");
-	QTimer *autoPlayTimer = new QTimer();
-
-	autoPlayTimer->setInterval(80);
-	//image play and vis=========================================================================================
-
-	QGroupBox *groupBox = new QGroupBox(tr("Parameters setting"));
+	QGroupBox *groupBox = new QGroupBox(tr("Import and Export"));
 	QVBoxLayout *vbox = new QVBoxLayout;
 	vbox->addLayout(secondRowLayout);
-	//vbox->addLayout(showImageDataLayout);
 	groupBox->setLayout(vbox);
-
-    
-    QDockWidget *dockRBGWidget = new QDockWidget();
-
-    dockRBGWidget->setWindowTitle("RGB");
-    addDockWidget(Qt::BottomDockWidgetArea, dockRBGWidget);
-    dockRBGWidget->setFloating(true);
-	//dockRBGWidget->setGeometry(920,418,500,430);
-	dockRBGWidget->hide();
 
     QDockWidget *dockPaletteWidget = new QDockWidget();
     PaletteViewWidget *paletteWidget = new PaletteViewWidget();
@@ -157,9 +95,6 @@ MainWindow::MainWindow(QWidget *parent) :
     addDockWidget(Qt::RightDockWidgetArea, dockPaletteWidget);
     dockPaletteWidget->setFloating(true);
 	dockPaletteWidget->setFeatures(QDockWidget::NoDockWidgetFeatures);
-
-    slider->setMinimum(0);
-    slider->setMaximum(0);
 
 	color_widgets::ColorDialog* dialog = new color_widgets::ColorDialog();
 	dialog->setWindowTitle("Color picker");
@@ -178,32 +113,22 @@ MainWindow::MainWindow(QWidget *parent) :
 	mainWidget->setLayout(mainLayout);
 	this->setCentralWidget(mainWidget);
 
-//    connect(showImageDataCheck, &QCheckBox::stateChanged, rgbWidget, &RGBWidget::setShowImageData);
-//    connect(showImageDataCheck, &QCheckBox::stateChanged, paletteWidget, &PaletteViewWidget::setShowImageData);
-
-//    connect(imagePreviewSlider, &QSlider::valueChanged, rgbWidget, &RGBWidget::setPreview);
- //   connect(imagePreviewSlider, &QSlider::valueChanged, paletteWidget, &PaletteViewWidget::setPreview);
-
     connect(OpenImageAndPaletteBtn, &QPushButton::clicked, [=](){this->openFile(true);});
     connect(CalcWeightsBtn, &QPushButton::clicked, data, &Data::ComputeWeights);
-
 	connect(dialog, &ColorDialog::colorChanged, paletteWidget, &PaletteViewWidget::getColor);
 	connect(paletteWidget, &PaletteViewWidget::setColor, dialog, &ColorDialog::setColor);
 	connect(RgbCalcPaletteButton, &QPushButton::clicked, [=]() {
 		int num = EditNum->text().toInt();
-		if (num < 1)
-		{
+		if (num < 1){
 			num = 6;
 			EditNum->setText(QString::number(num));
 		}
-
 		if (data)
 			data->ExtractPaletteWithKmenas(num);
 	});
 
 	connect(exportImageeBtn, &QPushButton::clicked, [=]() { this->exportImage(); });
-	connect(exportpsBtn, &QPushButton::clicked, [=]() { this->exportps(); });//ÐÂ¼ÓµÄ
-
+	connect(exportpsBtn, &QPushButton::clicked, [=]() { this->exportps(); });//æ–°åŠ çš„
 	connect(importPaletteButton, &QPushButton::clicked, [=]() { this->importPalette(); });
 	connect(exportPaletteButton, &QPushButton::clicked, [=]() { this->exportPalette(); });
 }
@@ -212,8 +137,7 @@ MainWindow::~MainWindow()
 {
 }
 
-// open image & poly file
-// TODO: replace input image file with H264 encoded image
+// open image 
 void MainWindow::openFile(bool merge){
     if(data == nullptr) return;
 
@@ -228,21 +152,16 @@ void MainWindow::openFile(bool merge){
 
         for(auto s : fileName){
 			image_file = s;
-            //data->OpenImage(QString(s));
         }
-		
     }
     else{
         return;
     }
 
     dialog.setNameFilter("*.data");
-
-    // if shows merge step, open several poly files instead of one
     dialog.setFileMode(merge ? QFileDialog::ExistingFiles : QFileDialog::ExistingFile);
 
-    if (dialog.exec())
-    {
+    if (dialog.exec()){
         QStringList fileName = dialog.selectedFiles();
 
         fileName.sort();
@@ -251,17 +170,12 @@ void MainWindow::openFile(bool merge){
             mergeStepSlider->setMaximum(fileName.size() - 1);
 
         int i = 0;
-        for(auto s : fileName)
-        {
-           // data->OpenPalette(s.toStdString());
+        for(auto s : fileName){
 			data_file = s;
             i++;
         }
-		//data->setNum(num);
 		data->OpenImage(image_file, data_file);
-
     }
-
 	imageBeforeDockWidget->show();
 	imageAfterDockWidget->show();
 }
@@ -272,8 +186,7 @@ void MainWindow::exportImage() {
 	QFileDialog fileDialog(this);
 	fileDialog.setFileMode(QFileDialog::DirectoryOnly);
 	QStringList dirName;
-	if (fileDialog.exec() == QDialog::Accepted)
-	{
+	if (fileDialog.exec() == QDialog::Accepted){
 		dirName = fileDialog.selectedFiles();
 		string dirNamestr = dirName[0].toStdString();
 
